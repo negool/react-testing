2e6ad060d7823d9cbb125d19fd7ebd8c44a619f3
Negar Tehranchi <negartehranchi@Negars-MacBook-Pro.local>
2022-02-27 22:27:18 -0500

    added search functionality

--- exercise-src/src/pages/People/PeopleTable/PeopleTable.js
+++ exercise-src/src/pages/People/PeopleTable/PeopleTable.js
@@ -1,10 +1,19 @@
 import { Table, TableThCell, TableCell, TableRow, TableContainer, TableFilterContainer } from 'components/Table';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import LoadingLogo from 'components/LoadingLogo';
 import Checkbox from 'components/Input/Checkbox';
 
-async function getPeople(param = '') {
-  return fetch(`http://localhost:4002/people?${param}`, { method: 'GET' }).then((response) =>
+// async function getPeople(params = '') {
+//   return fetch(`http://localhost:4002/people?${params}`, { method: 'GET' }).then((response) =>
+//     response.json()
+//   );
+// }
+async function getPeople(searchFilters) {
+  let params = searchFilters.filters.join('&');
+  if (searchFilters.search) {
+    params += `&name_like=${searchFilters.search}`;
+  }
+  return fetch(`http://localhost:4002/people?${params}`, { method: 'GET' }).then((response) =>
     response.json()
   );
 }
@@ -15,30 +24,61 @@ async function getPeople(param = '') {
 export default function PeopleTable() {
   const [people, setPeople] = useState(null);
   const [filters, setFilters] = useState([]);
+  const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    async function loadPeople() {
-      const people = await getPeople();
-      setPeople(people);
-    }
-    loadPeople();
+  const initialFilters = [];
+  const initialSearch = '';
+
+  const doGet = useCallback(async (params) => {
+    setPeople(await getPeople(params));
   }, []);
 
-  const handleCheckboxChange = (e) => {
-    const target = e.target;
-    let filterList = filters;
-    let curFilter = `${target.dataset.type}=${target.name}`;
-    if (target.checked) {
-      filterList.push(curFilter);
-    } else {
-      filterList = filters.filter((value, index, array) => {
-        return value !== curFilter;
-      });
-    }
-    setFilters(filterList);
-    getPeople(filterList.join('&')).then((ppl) => setPeople(ppl));
+  useEffect(() => {
+    doGet({
+      filters: initialFilters,
+      search: initialSearch
+    });
+  }, []);
 
+  const handleFilterChange = (filter) => {
+    filters.includes(filter)
+      ? setFilters(filters.filter((value) => value !== filter))
+      : setFilters(filters.concat(filter));
   }
+
+  const timeout = useRef();
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    const params = {
+      filters,
+      search
+    };
+    if (timeout.current) clearTimeout(timeout.current);
+
+    timeout.current = setTimeout(() => doGet(params), 100);
+    // setTimeout(() => doGet(params), 1000);
+  }
+  // const timeout = React.useRef();
+
+  // function onSearchChange(action, state) {
+  //   const params = {
+  //     search: state.search,
+  //   };
+
+  //   if (timeout.current) clearTimeout(timeout.current);
+
+  //   timeout.current = setTimeout(() => doGet(params), 500);
+  // }
+
+  useEffect(() => {
+    const params = {
+      filters,
+      search
+    };
+    doGet(params);
+  }, [filters]);
+
+  
 
   if (!people) {
     return <LoadingLogo />;
@@ -49,23 +89,25 @@ export default function PeopleTable() {
       <TableContainer>
         <TableFilterContainer>
           <div>
-            <label htmlFor="search">Search by Name:</label>
-            <input id="search" type="text" name="search" />
+            <label htmlFor="name">Search by Name:</label>
+            <input id="name" type="text" name="name" value={search} onChange={handleSearchChange}/>
           </div>
           <div>
             <Checkbox
               id="contractor"
               name="contractor"
-              onChange={handleCheckboxChange}
+              onChange={() => handleFilterChange('employment=contractor')}
               data-type="employment"
               label="Contractor"
+              checked={filters.includes('employment=contractor')}
             />
             <Checkbox
               id="employee"
               name="employee"
-              onChange={handleCheckboxChange}
+              onChange={() => handleFilterChange('employment=employee')}
               data-type="employment"
               label="Employee"
+              checked={filters.includes('employment=employee')}
             />
           </div>
         </TableFilterContainer>
